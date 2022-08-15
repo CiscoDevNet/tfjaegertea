f576b760865415acebc446f3e381f7949c441242
prathjan <prathjan@cisco.com>
2022-08-15 11:20:25 -0700

    use v1 of app

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ variable "ikswsname" {
 resource helm_release teaiksfrtfcb {
   name       = "teaiksapp"
   namespace = "default"
-  chart = "https://prathjan.github.io/helm-chart/jaegertea-1.0.0.tgz"
+  chart = "https://prathjan.github.io/helm-chart/jaegertea-1.0.1.tgz"
 
   set {
     name  = "MESSAGE"
